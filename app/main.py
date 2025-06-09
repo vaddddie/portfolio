95f23e156533c69b3a8b0dff8b7669d7c2805ed6
unknown <v_bykovski@mail.ru>
2025-06-09 14:34:48 +0300

    bug fixed

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.api.v1 import index
 from app.api.v1 import admin
 from app.db.controller import create_tables
 
-# create_tables()
+create_tables()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")

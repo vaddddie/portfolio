ef66ce68a66cebde1c0bfc39ae5995c30f4a7335
unknown <v_bykovski@mail.ru>
2025-07-10 17:32:33 +0300

    bug fixed

--- app/auth.py
+++ app/auth.py
@@ -3,7 +3,7 @@ from authx import AuthXConfig, AuthX
 auth_config = AuthXConfig()
 auth_config.JWT_SECRET_KEY = "123"
 auth_config.JWT_ACCESS_COOKIE_NAME = "access_token"
-auth_config.JWT_TOKEN_LOCATION = ['headers']
+auth_config.JWT_TOKEN_LOCATION = ['cookies']
 
 security = AuthX(config=auth_config)
 

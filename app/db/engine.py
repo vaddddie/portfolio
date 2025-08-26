472547be89d7192057216ba3ab7f69462754e98d
unknown <v_bykovski@mail.ru>
2025-08-26 14:45:50 +0300

    bug fixed

--- app/db/engine.py
+++ app/db/engine.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-Base = declarative_base()
-engine = create_engine(f'postgresql://user:password@db:5432/mydatabase', echo=False)
+DATABASE_URL = 'postgresql://user:password@db:5432/mydatabase'
+# Base = declarative_base()
+# engine = create_engine(f'postgresql://user:password@db:5432/mydatabase', echo=False)
+engine = create_async_engine(DATABASE_URL, echo=True)
+Session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-Session = sessionmaker(bind=engine)
+# Session = sessionmaker(bind=engine)
     
 

e3a9e17b8aa0c978440d23ff4741fa7a1e6f176a
unknown <v_bykovski@mail.ru>
2025-09-03 14:42:09 +0300

    bug fixed

--- app/adminPanel.py
+++ app/adminPanel.py
@@ -31,7 +31,7 @@ class ProjectAdmin(ModelView, model=Project):
     can_edit = True
     can_delete = True
 
-class ProjectImageAdmin(ModelView, model=User):
+class ProjectImageAdmin(ModelView, model=ProjectImage):
     column_list = [ProjectImage.id, ProjectImage.project_id, ProjectImage.image_url]
     column_searchable_list = [ProjectImage.project_id]
     can_create = True
